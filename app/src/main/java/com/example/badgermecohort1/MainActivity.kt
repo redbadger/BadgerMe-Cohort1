f04f76b240ddaa9ad9a43bb6ebf644efd3ed22f4
Tim Lee <tim.lee@red-badger.com>
2022-04-21 17:28:43 +0100

    Add loading spinner

--- app/src/main/java/com/example/badgermecohort1/MainActivity.kt
+++ app/src/main/java/com/example/badgermecohort1/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.example.badgermecohort1
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.os.Bundle
-import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.animation.core.*
 import com.example.badgermecohort1.ui.theme.BadgerMeCohort1Theme
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import kotlinx.coroutines.delay
+import androidx.compose.ui.unit.dp
+import java.lang.Math.toRadians
+import kotlin.math.cos
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,20 +39,18 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun SplashScreenAnimate() {
-    val scale = remember { Animatable(0f) }
-    LaunchedEffect(key1 = true) {
-        scale.animateTo(
-            targetValue = 0.5f,
-            animationSpec = tween(
-                durationMillis = 500,
-                easing = {
-                    OvershootInterpolator(2f).getInterpolation(it)
-                }
-            )
+    val numberOfBars = 12
+    val barValues = remember { mutableStateOf(1..numberOfBars)}
+    val infiniteTransition = rememberInfiniteTransition()
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1200, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
         )
+    )
 
-        delay(3000L)
-    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -68,8 +66,42 @@ fun SplashScreenAnimate() {
             contentDescription = "Splash Image",
             Modifier.padding(Dp(10.0F))
         )
+        Canvas(modifier = Modifier
+            .height(100.dp)
+            .width(100.dp)
+            .padding(20.dp)) {
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+            val barWidth = 10F
+            val barHeight = 30F
+            val maxAlpha = 200
+            val minAlpha = 20
+            val alphaRange = (maxAlpha - minAlpha).toFloat()
+            val baseColour = 100;
+            val barSpread = 60F;
+            val cornerRadius = 5;
+
+            barValues.value.forEach {
+                val degrees = (360F / numberOfBars) * (it - 1F)
+                val degreesRad = toRadians(degrees.toDouble()).toFloat()
+                val alpha = (alphaRange * (scale + it / 12F)).mod(alphaRange).toInt() + minAlpha;
+
+                withTransform({
+                    translate(top = -barSpread * cos(degreesRad), left = barSpread * sin(degreesRad))
+                    rotate(degrees = degrees)
+                }) {
+                    drawRoundRect(
+                        color = Color(baseColour, baseColour, baseColour, alpha),
+                        topLeft = Offset((canvasWidth - barWidth) / 2F, canvasHeight / 2F),
+                        size = Size(barWidth, barHeight),
+                        cornerRadius = CornerRadius(
+                            x = cornerRadius.dp.toPx(),
+                            y = cornerRadius.dp.toPx()
+                        )
+                    )
+                }
+            }
+        }
         Text(text = "Loading")
     }
-
-
 }
