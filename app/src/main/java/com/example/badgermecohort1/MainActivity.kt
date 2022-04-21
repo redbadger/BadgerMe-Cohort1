1533f5a4bdbad98a4a0ee54d7d82416092d25655
Deen Bashir <23169347+DBashir@users.noreply.github.com>
2022-04-21 10:40:59 +0100

    splash screen basic implementation

--- app/src/main/java/com/example/badgermecohort1/MainActivity.kt
+++ app/src/main/java/com/example/badgermecohort1/MainActivity.kt
@@ -1,43 +1,75 @@
 package com.example.badgermecohort1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
 import com.example.badgermecohort1.ui.theme.BadgerMeCohort1Theme
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BadgerMeCohort1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SplashScreenAnimate()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BadgerMeCohort1Theme {
-        Greeting("Android")
+fun SplashScreenAnimate() {
+    val scale = remember { Animatable(0f) }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.5f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+
+        delay(3000L)
     }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.rb_title),
+            contentDescription = "rb title",
+            modifier = Modifier.padding(Dp(50.0F))
+        )
+        Image(
+            painter = painterResource(R.drawable.rb_logo),
+            contentDescription = "Splash Image",
+            Modifier.padding(Dp(10.0F))
+        )
+        Text(text = "Loading")
+    }
+
+
 }
