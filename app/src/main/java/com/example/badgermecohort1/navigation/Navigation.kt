60ecc63abe61b33263d72c0abbacc7db73d50923
Deen Bashir <23169347+DBashir@users.noreply.github.com>
2022-04-25 11:52:13 +0100

    login screen UI implemented

--- app/src/main/java/com/example/badgermecohort1/navigation/Navigation.kt
+++ app/src/main/java/com/example/badgermecohort1/navigation/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -18,10 +19,7 @@ fun navigation() {
             SplashScreenAnimate(navController)
         }
         composable("main_screen") {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-                Text(text = "Main screen", modifier = Modifier.fillMaxSize())
+            login()
             }
         }
     }
-
-}
