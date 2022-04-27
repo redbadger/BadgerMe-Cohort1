8354da9e9cfb341b3a3a312241c640170a853dd0
Tim Lee <tim.lee@red-badger.com>
2022-04-27 14:02:34 +0100

    Silent sign in

--- app/src/main/java/com/example/badgermecohort1/api/ApiInterface.kt
+++ app/src/main/java/com/example/badgermecohort1/api/ApiInterface.kt
@@ -1,6 +1,11 @@
 package com.example.badgermecohort1.api
 
+import android.util.Log
 import com.example.badgermecohort1.model.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.tasks.Task
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import retrofit2.Call
@@ -17,24 +22,23 @@ interface ApiInterface {
 
         var BASE_URL = "https://meek-hummingbird-b2e163.netlify.app/api/"
 
-        fun create() : ApiInterface {
-
-            val token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg2MTY0OWU0NTAzMTUzODNmNmI5ZDUxMGI3Y2Q0ZTkyMjZjM2NkODgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzNzM5NzEzODEyMzQtN3ZwczViZTcyZGNjdmcxYmV2bzlmdTg5aHY3c241N2kuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIzNzM5NzEzODEyMzQtaWRhdGlvZWlvZDRiYXRqbjhhcW1sdTFxdjhiYWs4ZHAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAxOTk3NjgzMzM2NTc4OTcyMjMiLCJoZCI6InJlZC1iYWRnZXIuY29tIiwiZW1haWwiOiJkZWVuLmJhc2hpckByZWQtYmFkZ2VyLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiRGVlbiBCYXNoaXIiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUFUWEFKd2xZUHNxT1FhUFRWUjhpekZqb1FULTRickZPWUtPRkl6NGtBLVA9czk2LWMiLCJnaXZlbl9uYW1lIjoiRGVlbiIsImZhbWlseV9uYW1lIjoiQmFzaGlyIiwibG9jYWxlIjoiZW4tR0IiLCJpYXQiOjE2NTEwNTU5NTAsImV4cCI6MTY1MTA1OTU1MH0.dIO3P5Sp3K8hY1JOoRUgDu4Tm0mAqQRTnaTV7AZH1RE78CL6IGTZkDoGSobzighOvLqPK5r8pkVPdGsrb-_WbAbWRWwP723u-LFtVFAngrSo7ZtwbpVSMCbGttcJzv0M7Q1iUKSZgFqN4qzSORiusp24iQ6KS0EL0Ev6D-tQ6OfiMkjo4iaPqYITRpXNoTyWjX5o8TOAWtWXxgrkJBY5op3kObLKpAss06xDASWlBj_AOA4spT9t1p7ucHZAq2Ud_ncI5Ft8EVk8hJPTDNFU2JWC5M-7DxIdNOdJBpH3m5eAEtwZgnizaiwBtmMqOAn4v7TrHGriR9pJRLLrID5wzQ"
+        fun create(token: String?): ApiInterface {
+//            val token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg2MTY0OWU0NTAzMTUzODNmNmI5ZDUxMGI3Y2Q0ZTkyMjZjM2NkODgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzNzM5NzEzODEyMzQtZThrcHZ1dnI3cDAxZWg5bWswdmtwbTIzazEyazFrcnIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIzNzM5NzEzODEyMzQtaWRhdGlvZWlvZDRiYXRqbjhhcW1sdTFxdjhiYWs4ZHAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDAzNjg4ODg4NjY0NjY2NjExNDYiLCJoZCI6InJlZC1iYWRnZXIuY29tIiwiZW1haWwiOiJ0aW0ubGVlQHJlZC1iYWRnZXIuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJUaW0gTGVlIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdocUJXUDdnQlktMk5oUWkxRWNmcW9JNFYxZnRVdm1HV2R2MXBMMT1zOTYtYyIsImdpdmVuX25hbWUiOiJUaW0iLCJmYW1pbHlfbmFtZSI6IkxlZSIsImxvY2FsZSI6ImVuLUdCIiwiaWF0IjoxNjUxMDU2Njc2LCJleHAiOjE2NTEwNjAyNzZ9.dc-I-UtAyhHFdszJW9wHwkgNzmcsxRDxcD0tS6yyvJdXRV-pjOUhpnm7_DpEh8VDbQ9Hxnz8Rxxsz_tbJikUlSNfUytKtcmhIeDHGiSrQAbkGD8bPOlLwwEZ_vraAccli6VGx7Q-0XxjZYhEKbqOLj2jjxzfFA_UaJVz_DqWaYE6vigkYubXOLo-iomjdxlUxbX1WwaG_EYxT8r8vanOueIAmmR1yoXxCljJWa3j0cNgXM_AIZaOgyKyavxOqGWGKB2s4FGEKMZsG4jaQIygCm2hc-X19pNMgPCI0XqHaMRbfC3pFhW2iHEvdxkY09fr8fNKPYhX3Jah4VhVWyHCvQ"
 
             val client = OkHttpClient.Builder().addInterceptor { chain ->
                 val newRequest: Request = chain.request().newBuilder()
-                    .addHeader("Authorization", "Bearer $token")
-                    .build()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
                 chain.proceed(newRequest)
             }.build()
 
 
             val retrofit = Retrofit.Builder()
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
                 .build()
             return retrofit.create(ApiInterface::class.java)
-
         }
     }
 
--- app/src/main/java/com/example/badgermecohort1/auth/GoogleSignInClient.kt
+++ app/src/main/java/com/example/badgermecohort1/auth/GoogleSignInClient.kt
@@ -12,7 +12,7 @@ fun getGoogleLoginAuth(context: Context): GoogleSignInClient {
     val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 //         Request id token if you intend to verify google user from your backend server
 //        .requestIdToken(context.getString(R.string.backend_client_id))
-        //.requestEmail()
+        .requestEmail()
         .requestIdToken("373971381234-idatioeiod4batjn8aqmlu1qv8bak8dp.apps.googleusercontent.com")
         .build()
 
